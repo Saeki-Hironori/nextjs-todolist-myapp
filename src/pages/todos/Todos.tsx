ac09c8e244085fa1c442a3f49616fd06d3460df8
Saeki Hironori <saeki.hironori@itfllc.co.jp>
2023-04-14 18:05:56 +0900

    更新

--- src/pages/todos/Todos.tsx
+++ src/pages/todos/Todos.tsx
@@ -12,6 +12,8 @@ import { app, db } from "@/lib/firebase";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { Box, Button, LinearProgress } from "@mui/material";
 import { query, orderBy } from "firebase/firestore";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
 
 type TODO = {
   id: string;
@@ -20,10 +22,19 @@ type TODO = {
   title: string;
 };
 
+type TODOArray = Array<{
+  id: string;
+  createdAt: Timestamp;
+  status: string;
+  title: string;
+}>;
+
 const Todos = () => {
   const [todos, setTodos] = useState<TODO[]>([]);
   const auth = getAuth(app);
   const [user, setUser] = useState(auth.currentUser);
+  const [filter, setFilter] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState<TODOArray>([]);
 
   //firestoreのデータをcreatedBy順にする
 
@@ -36,6 +47,10 @@ const Todos = () => {
     });
     AllData();
   }, [auth]);
+
+  useEffect(() => {
+    filteringTodos();
+  }, [filter, todos]);
 
   // 全てのデータを表示する（タイムスタンプ順）
   const AllData = async () => {
@@ -62,37 +77,96 @@ const Todos = () => {
     AllData();
   };
 
+  const filteringTodos = () => {
+    switch (filter) {
+      case "notStarted":
+        setFilteredTodos(todos.filter((todo) => todo.status === "notStarted"));
+        break;
+      case "doing":
+        setFilteredTodos(todos.filter((todo) => todo.status === "doing"));
+        break;
+      case "done":
+        setFilteredTodos(todos.filter((todo) => todo.status === "done"));
+        break;
+      default:
+        setFilteredTodos(todos);
+    }
+  };
+
   return (
     <>
       {user ? (
         <>
           <Header />
-          <div style={{ maxWidth: "50em", margin: "2em", fontSize: "20px" }}>
-            <h1>Todo一覧</h1>
-            <div>
-              {todos.map((todo) => (
-                <li key={todo.id} className="list">
-                  <select
-                    value={todo.status}
-                    onChange={(e: React.ChangeEvent<HTMLSelectElement>) => {
-                      statusChange(todo, e);
-                    }}
-                  >
-                    <option value="notStarted">未着手</option>
-                    <option value="doing">進行中</option>
-                    <option value="done">完了</option>
-                  </select>
-                  <span className="todo_title">{todo.title}</span>
-                  <button>編集</button>
-                  <button>削除</button>
-                </li>
-              ))}
+          <div>
+            <h1>TodoList</h1>
+            <div className="filter">
+              <label>Pick Up : </label>
+              <select
+                value={filter}
+                onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
+                  setFilter(e.target.value)
+                }
+              >
+                <option value="all">すべて</option>
+                <option value="notStarted">未着手</option>
+                <option value="doing">進行中</option>
+                <option value="done">完了</option>
+              </select>
             </div>
-            <AddTodos />
-            <div style={{ marginTop: "20px" }}>
-              <Button onClick={AllData} color={"success"} fullWidth>
-                データ同期
-              </Button>
+          </div>
+          <div style={{ margin: "1em 2em" }}>
+            <div
+              style={{
+                maxWidth: "50em",
+                margin: "0 auto",
+                fontSize: "20px",
+              }}
+            >
+              {filteredTodos.map((todo) => (
+                <Box
+                  sx={{
+                    display: "flex",
+                    bgcolor: "background.paper",
+                    borderRadius: 1,
+                  }}
+                >
+                  <li key={todo.id} className="list">
+                    <select
+                      value={todo.status}
+                      onChange={(e: React.ChangeEvent<HTMLSelectElement>) => {
+                        statusChange(todo, e);
+                      }}
+                      style={{
+                        marginRight: "10px",
+                      }}
+                    >
+                      <option value="notStarted">未着手</option>
+                      <option value="doing">進行中</option>
+                      <option value="done">完了</option>
+                    </select>
+                    <span className="todo_title">{todo.title}</span>
+                    <Button>編集</Button>
+                    <Button>削除</Button>
+                  </li>
+                </Box>
+              ))}
+              <AddTodos />
+              <div
+                style={{
+                  marginTop: "20px",
+                  color: "red",
+                }}
+              >
+                <Button
+                  onClick={AllData}
+                  color="success"
+                  variant="outlined"
+                  fullWidth
+                >
+                  データ同期
+                </Button>
+              </div>
             </div>
           </div>
         </>
